1c3427f781b14fe2abcf3c9d5cd1350b5b83f22d
admin <303764106@qq.com>
2020-02-24 18:43:05 +0800

    aaa

--- helloworld/src/com/itheima/helloworld.java
+++ helloworld/src/com/itheima/helloworld.java
@@ -2,6 +2,8 @@ package com.itheima;
 
 public class helloworld {
     public static void main(String[] args) {
+
         System.out.println("hello 你好");
+        System.out.println("nihao");
     }
 }
